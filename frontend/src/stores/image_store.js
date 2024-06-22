e0539cb7388236bf9d1682005c38e974ca155a8d
Pedro <pdihax@gmail.com>
2024-06-22 02:44:14 -0300

    Add options to select tile sizes

--- frontend/src/stores/image_store.js
+++ frontend/src/stores/image_store.js
@@ -1,7 +1,9 @@
 import { writable } from 'svelte/store';
 
 let image = writable(null);
+let image_width = writable(0);
+let image_height = writable(0);
 let tiles = writable([]);
 let selected = writable(-1);
 
-export { image, tiles, selected };
+export { image, image_width, image_height, tiles, selected };
