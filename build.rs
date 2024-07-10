76a91465fd5280a8c810293d4b929ab924f10eaf
Pedro Lopez <pdihax@gmail.com>
2024-07-10 01:23:32 -0300

    [Backend] Add build script to build static page before running the server

---
+++ build.rs
@@ -0,0 +1,50 @@
+fn main() {
+    // Check if npm in installed
+    let npm_installed = std::process::Command::new("npm")
+        .arg("--version")
+        .output()
+        .is_ok();
+
+    if !npm_installed {
+        panic!("npm is not installed");
+    }
+
+    // Run npm install in the frontend directory
+    let status = std::process::Command::new("npm")
+        .arg("install")
+        .current_dir("frontend")
+        .status()
+        .expect("npm install failed");
+
+    if !status.success() {
+        panic!("npm install failed");
+    }
+
+    // Run npm run build in the frontend directory
+    let status = std::process::Command::new("npm")
+        .arg("run")
+        .arg("build")
+        .current_dir("frontend")
+        .status()
+        .expect("npm run build failed");
+
+    if !status.success() {
+        panic!("npm run build failed");            
+    }
+
+    // Copy contents from frontend/dist to /static directory
+    let status = std::process::Command::new("cp")
+        .arg("-r")
+        .arg("frontend/dist/.")
+        .arg("static/")
+        .status()
+        .expect("copy failed");
+
+    if !status.success() {
+        panic!("copy failed");
+    }
+
+    println!("cargo:rerun-if-changed=frontend");
+
+
+}
